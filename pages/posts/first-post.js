fcb8ac803e771d63a9b8958e2e129981607c6a2e
Renovatio3d <renovatio3d@gmail.com>
2023-01-12 23:29:36 -0500

    Initial commit

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 export default function FirstPost() {
     return (
       <>
+      
         <h1>First Post</h1>
         <h2>
           <Link href="/">Back to home</Link>
